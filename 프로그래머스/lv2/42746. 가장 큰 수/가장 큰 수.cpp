3ba2f7a898aa68e8069987a04f511494b7b7ab86
Saeyeon Yoo <42169002+saeyeondal@users.noreply.github.com>
2022-06-12 22:28:11 +0900

    [level 2] Title: 가장 큰 수, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool cmp(string a, string b){
+    return a+b>b+a;
+}
+
+string solution(vector<int> numbers) {
+    string answer = "";
+    vector<string> n;
+    int sum=0;
+    for(int i=0; i<numbers.size(); i++){
+        n.push_back(to_string(numbers[i]));
+    }
+    sort(n.begin(), n.end(), cmp);
+    for(int i=0; i<n.size(); i++){
+        answer+=n[i];
+        sum=sum+numbers[i];
+    }
+    if(sum==0) return "0"; 
+    return answer;
+}
