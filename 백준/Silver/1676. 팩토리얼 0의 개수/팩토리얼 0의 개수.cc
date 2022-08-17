bfc1b03828d0a58e1df7dc1958beac3d3c4d6817
Saeyeon Yoo <42169002+saeyeondal@users.noreply.github.com>
2022-08-17 17:31:57 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.cc
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.cc
@@ -3,26 +3,19 @@
 using namespace std;
 
 int main() {
-	int n, cnt_two=0, cnt_five=0;
+	int n, cnt=0;
 	cin>>n;
-	//2*5의 개수 구하기 
+	//5의 개수 구하기 
 	for(int i=n; i>=1; i--){
 		int tmp=i; 
 		while(1){
-			if(tmp%2==0) {
-				cnt_two++;	
-				tmp=tmp/2;
-			}
-			else if(tmp%5==0) {
-				cnt_five++;
+			if(tmp%5==0) {
+				cnt++;
 				tmp=tmp/5;
 			}	
 			else break;		
 		}
 	} 
-	if(cnt_two>0 && cnt_five>0){
-		cout<<min(cnt_two, cnt_five);
-	}
-	else cout<<0;
+	cout<<cnt;
 	return 0;
 }
